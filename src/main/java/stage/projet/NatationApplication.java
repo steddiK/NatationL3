80b1d0616cc312a7064b93327b3b356024be6358
Steddi <steddi.andritiana@pulse.mg>
2024-03-06 14:03:44 +0300

    premier commit

---
+++ src/main/java/stage/projet/NatationApplication.java
@@ -0,0 +1,13 @@
+package stage.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stage/projet/NatationApplicationTests.java
@@ -0,0 +1,13 @@
+package stage.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
