3e0f0568eaf9a0f1b27dc81239c0e129654a9c90
Steddi <steddi.andritiana@pulse.mg>
2024-03-07 09:00:39 +0300

    getter setter

--- src/main/java/stage/projet/entity/competition.java
+++ src/main/java/stage/projet/entity/competition.java
@@ -11,45 +11,19 @@ import java.sql.Date;
 @Entity
 @Transactional
 @Table(name= "COMPETITION")
+@Getter
+@Setter
 public class competition {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private  int IdCompet;
-
-    @Setter
-    @Getter
     private String NomCompet;
-
-    @Setter
-    @Getter
     private String Lieu;
-
-    @Setter
-    @Getter
     private String TypeCompet;
-
-    @Setter
-    @Getter
     private String Sexe;
-
-
-    @Getter
-    @Setter
     private String niveau;
-
-    @Getter
-    @Setter
     private float score;
-
-    @Getter
-    @Setter
     private int rang;
-
-    @Getter
-    @Setter
     private Date date;
-
-    @Getter
-    @Setter
     private String heure;
 }
