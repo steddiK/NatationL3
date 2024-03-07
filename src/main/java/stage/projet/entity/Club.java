a8eae5e19bc79154b9b7d6fdb58805a192aee5d7
Nombana <avo.andrianjohany@pulse.com>
2024-03-07 08:23:59 +0300

    création d' entité Club

--- src/main/java/stage/projet/entity/Club.java
+++ src/main/java/stage/projet/entity/Club.java
@@ -1,5 +1,23 @@
 package stage.projet.entity;
 
-public class Club {
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Entity
+@Transactional
+@Table(name="CLUB")
+public class Club {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int idClub;
+    @Setter
+    @Getter
+    private String NomClub;
+    @Setter
+    @Getter
+    private int TelClub;
 }
