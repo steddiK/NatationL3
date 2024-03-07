c63b4d2a867d4a02657dfda9d9b9ad07fd1d190e
Steddi <steddi.andritiana@pulse.mg>
2024-03-07 08:55:05 +0300

    tapany

--- src/main/java/stage/projet/entity/competition.java
+++ src/main/java/stage/projet/entity/competition.java
@@ -1,5 +1,35 @@
 package stage.projet.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+import java.sql.Date;
+
+@Entity
+@Transactional
+@Table(name= "COMPETITION")
 public class competition {
 
+    @Getter
+    @Setter
+    private String niveau;
+
+    @Getter
+    @Setter
+    private float score;
+
+    @Getter
+    @Setter
+    private int rang;
+
+    @Getter
+    @Setter
+    private Date date;
+
+    @Getter
+    @Setter
+    private String heure;
 }
